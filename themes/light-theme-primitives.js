f1674e283bc5ed6a7d2e522a541f34bd7aeeb625
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-04-29 17:47:50 +0700

    feat: add initial our tokens

--- themes/light-theme-primitives.js
+++ themes/light-theme-primitives.js
@@ -1,4 +1,14 @@
 export const primitives = {
   // TODO: Fill me: https://baseweb.design/theming/theming-values/
-  primaryFontFamily: '"Inter", system-ui, "Helvetica Neue", Arial, sans-serif'
+  primaryFontFamily: '"Inter", system-ui, "Helvetica Neue", Arial, sans-serif',
+
+  $colors: {
+    red: '#E53A40',
+    redDarker: '#D01C22',
+    navy: '#001B44',
+    blackLighter: '#333333',
+    black: '#000000',
+    silver: '#EEEEEE',
+    gray: '#DDDDDD'
+  }
 }
