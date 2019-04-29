b7144711f29eb510437e961b5d98127dc7ca950c
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-04-29 17:48:33 +0700

    feat: create initial page layout for docs

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,40 @@
-import React from 'react'
 import App, { Container } from 'next/app'
-import { Provider as StyletronProvider } from 'styletron-react'
+import { Provider as StyletronProvider, styled } from 'styletron-react'
 import { styletron } from '../styletron'
+
+import Column from '../components/Grid/Column'
+import Sidebar from '../components/docs/Sidebar'
+
+const Content = styled('main', {
+
+  padding: '0 1rem',
+
+  ':before, :after': {
+    clear: 'both',
+    content: '',
+    display: 'table'
+  },
+
+  '@media screen and (min-width: 60em)': {
+    padding: 0,
+    marginLeft: '380px',
+    marginRight: '120px'
+  }
+})
 
 export default class MyApp extends App {
   render () {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps, router } = this.props
+
     return (
       <Container>
         <StyletronProvider value={styletron}>
-          <Component {...pageProps} />
+          <Sidebar path={router.pathname} />
+          <Content>
+            <Column size={10}>
+              <Component {...pageProps} />
+            </Column>
+          </Content>
         </StyletronProvider>
       </Container>
     )
