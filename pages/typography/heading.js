5e7e24f99493a450de7699e543ee10c5daabf56a
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-04-30 15:53:01 +0700

    feat(pages/typography): add example

--- pages/typography/heading.js
+++ pages/typography/heading.js
@@ -1,7 +1,78 @@
-import { H3 } from 'baseui/typography'
+import { styled } from 'styletron-react'
+import { H1, H2, H3, H4, H5, H6, Paragraph1 } from 'baseui/typography'
+import { Card } from 'baseui/card'
+
+const CardContainer = styled('div', {
+  marginBottom: '4rem'
+})
 
 export default () => (
   <>
     <H3>Heading</H3>
+    <CardContainer>
+      <Paragraph1>
+        48px — H1 Heading
+      </Paragraph1>
+      <Card>
+        <H1>
+          The Quick Brown Fox Jumps Over the Lazy Dog
+        </H1>
+      </Card>
+    </CardContainer>
+
+    <CardContainer>
+      <Paragraph1>
+        36px — H2 Heading
+      </Paragraph1>
+      <Card>
+        <H2>
+          The Quick Brown Fox Jumps Over the Lazy Dog
+        </H2>
+      </Card>
+    </CardContainer>
+
+    <CardContainer>
+      <Paragraph1>
+        24px — H3 Heading
+      </Paragraph1>
+      <Card>
+        <H3>
+          The Quick Brown Fox Jumps Over the Lazy Dog
+        </H3>
+      </Card>
+    </CardContainer>
+
+    <CardContainer>
+      <Paragraph1>
+        20px — H4 Heading
+      </Paragraph1>
+      <Card>
+        <H4>
+          The Quick Brown Fox Jumps Over the Lazy Dog
+        </H4>
+      </Card>
+    </CardContainer>
+
+    <CardContainer>
+      <Paragraph1>
+        16px — H5 Heading (base)
+      </Paragraph1>
+      <Card>
+        <H5>
+          The Quick Brown Fox Jumps Over the Lazy Dog
+        </H5>
+      </Card>
+    </CardContainer>
+
+    <CardContainer>
+      <Paragraph1>
+        14px — H6 Heading
+      </Paragraph1>
+      <Card>
+        <H6>
+          The Quick Brown Fox Jumps Over the Lazy Dog
+        </H6>
+      </Card>
+    </CardContainer>
   </>
 )
--- pages/typography/index.js
+++ pages/typography/index.js
@@ -1,7 +1,17 @@
-import { H2 } from 'baseui/typography'
+import { H2, Paragraph2 } from 'baseui/typography'
 
 export default () => (
   <>
     <H2>Typography</H2>
+
+    <Paragraph2>
+      Typography is the heart of the web. Almost 70% of website content was text. Good
+      Typography is a good design, and good design is a good business.
+    </Paragraph2>
+
+    <Paragraph2>
+      We have 3 typefaces and our favorite ones was Inter UI for Sans Serif, and Helvetica
+      for the fallback. We use Merriweather for Serif, and Space Mono for Monospace.
+    </Paragraph2>
   </>
 )
