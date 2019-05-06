b55db46bd73620dbfcf104bf11c8e8dc4737f4ac
ri7nz <ri7nz.labs@gmail.com>
2019-05-06 23:27:56 +0700

    [Fixed] Jasmine for Style Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,48 +1,51 @@
-import App, { Container } from 'next/app'
-import { Provider as StyletronProvider, styled } from 'styletron-react'
-import { styletron } from '../styletron'
+import App, { Container } from "next/app";
+import { Provider as StyletronProvider, styled } from "styletron-react";
+import { styletron } from "../styletron";
+import { ThemeProvider } from "baseui";
+import Jasmine from "../themes";
 
-import Column from '../components/Grid/Column'
-import Sidebar from '../components/docs/Sidebar'
+import Column from "../components/Grid/Column";
+import Sidebar from "../components/docs/Sidebar";
 
-const Content = styled('main', {
+const Content = styled("main", {
+  padding: "0 1rem",
 
-  padding: '0 1rem',
-
-  ':before': {
-    clear: 'both',
+  ":before": {
+    clear: "both",
     content: '""',
-    display: 'table'
+    display: "table"
   },
 
-  ':after': {
-    clear: 'both',
+  ":after": {
+    clear: "both",
     content: '""',
-    display: 'table'
+    display: "table"
   },
 
-  '@media screen and (min-width: 60em)': {
+  "@media screen and (min-width: 60em)": {
     padding: 0,
-    marginLeft: '380px',
-    marginRight: '120px'
+    marginLeft: "380px",
+    marginRight: "120px"
   }
-})
+});
 
 export default class MyApp extends App {
-  render () {
-    const { Component, pageProps, router } = this.props
+  render() {
+    const { Component, pageProps, router } = this.props;
 
     return (
       <Container>
         <StyletronProvider value={styletron}>
-          <Sidebar path={router.pathname} />
-          <Content>
-            <Column size={10}>
-              <Component {...pageProps} />
-            </Column>
-          </Content>
+          <ThemeProvider theme={Jasmine}>
+            <Sidebar path={router.pathname} />
+            <Content>
+              <Column size={10}>
+                <Component {...pageProps} />
+              </Column>
+            </Content>
+          </ThemeProvider>
         </StyletronProvider>
       </Container>
-    )
+    );
   }
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,39 +1,35 @@
-import Document, { Head, Main, NextScript } from 'next/document'
-import { Provider as StyletronProvider } from 'styletron-react'
-import { ThemeProvider } from 'baseui'
-import { styletron } from '../styletron'
-
-import Jasmine from '../themes'
+import Document, { Head, Main, NextScript } from "next/document";
+import { Provider as StyletronProvider } from "styletron-react";
+import { ThemeProvider } from "baseui";
+import { styletron } from "../styletron";
 
 class MyDocument extends Document {
-  static getInitialProps (props) {
+  static getInitialProps(props) {
     const page = props.renderPage(App => props => (
       <StyletronProvider value={styletron}>
-        <ThemeProvider theme={Jasmine}>
-          <App {...props} />
-        </ThemeProvider>
+        <App {...props} />
       </StyletronProvider>
-    ))
-    const stylesheets = styletron.getStylesheets() || []
-    return { ...page, stylesheets }
+    ));
+    const stylesheets = styletron.getStylesheets() || [];
+    return { ...page, stylesheets };
   }
 
-  render () {
+  render() {
     return (
       <html>
         <Head>
           {this.props.stylesheets.map((sheet, i) => (
             <style
-              className='_styletron_hydrate_'
+              className="_styletron_hydrate_"
               dangerouslySetInnerHTML={{ __html: sheet.css }}
               media={sheet.attrs.media}
-              data-hydrate={sheet.attrs['data-hydrate']}
+              data-hydrate={sheet.attrs["data-hydrate"]}
               key={i}
             />
           ))}
           <link
-            rel='stylesheet'
-            href='https://cdn.jsdelivr.net/npm/inter-ui@3.5.0/inter.min.css'
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/inter-ui@3.5.0/inter.min.css"
           />
         </Head>
         <body>
@@ -41,8 +37,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
