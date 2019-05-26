f8216c9b2f8c1135aea692b4ab55fccbb505ed3e
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-05-26 08:36:35 +0700

    refactor: move to directory

--- themes/light-theme.js
+++
@@ -1,7 +0,0 @@
-const { primitives } = require('./light-theme-primitives')
-
-// import { primitives } from './light-theme-primitives.js'
-
-module.exports = { primitives }
-
-// export { primitives }
