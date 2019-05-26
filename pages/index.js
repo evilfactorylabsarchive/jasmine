c7dd12aba03546efc4d76939e1b9b5f7e899edb4
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-05-26 08:36:18 +0700

    styles: formatted with pretteir

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,15 @@ import { Paragraph2, H2, H4 } from 'baseui/typography'
 
 export default () => (
   <>
-    <H2>
-      jasmine
-    </H2>
+    <H2>jasmine</H2>
     <Notification
       kind={KIND.warning}
-      overrides={{ Body: { style: { width: 'auto' }} }}
+      overrides={{ Body: { style: { width: 'auto' } } }}
     >
-      jasmine is evilfactory UI Components built on top baseweb. Currently under heavy development
-      and not stable yet
+      jasmine is evilfactory UI Components built on top baseweb. Currently under
+      heavy development and not stable yet
     </Notification>
     <H4>TODO:</H4>
-    <Paragraph2>
-      Use MDX instead for this docs.
-    </Paragraph2>
+    <Paragraph2>Use MDX instead for this docs.</Paragraph2>
   </>
 )
